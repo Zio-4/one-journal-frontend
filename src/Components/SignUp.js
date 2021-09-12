237e90748460839d37582b0fc297ac04d4656f49
Philip Ziolkowski <philipz4848@gmail.com>
2021-09-11 17:38:36 -0700

    creating signup component

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -7,32 +7,31 @@ function SignUp() {
                 <div class="column">
                     <h2 class="ui orange header">
                         <div class="content">
-                            Log-in to your account
+                            Create a new account
                         </div>
                     </h2>
                     <form class="ui large form">
                         <div class="ui stacked segment">
                             <div class="field">
-                            <div class="ui left icon input">
-                                <i class="user icon"></i>
-                                <input type="text" name="username" placeholder="Username"/>
-                            </div>
+                                    <input type="text" name="username" placeholder="Username"/>
                             </div>
                             <div class="field">
-                            <div class="ui left icon input">
-                                <i class="lock icon"></i>
                                 <input type="password" name="password" placeholder="Password"/>
                             </div>
+                            <div class="field">
+                                <input type="password" name="confirm-password" placeholder="Confirm Password"/>
                             </div>
-                            <div class="ui fluid large orange submit button">Login</div>
+                            <div class="field">
+                                <input type="text" name="person-name" placeholder="First Name"/>
+                            </div>
+                            <div class="ui fluid large orange submit button">Sign Up</div>
                         </div>
 
                         <div class="ui error message"></div>
-
                     </form>
 
                     <div class="ui message">
-                        Don't have an account? <a href="#"> Sign Up</a>
+                        Already have an account? <a href="#"> Sign In</a>
                     </div>
                 </div>
             </div>
